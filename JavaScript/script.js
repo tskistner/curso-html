80b229407f8ee3db7c95246f48d2f4908b5e8f96
vitorgrilo96 <vitorluizalcantaragrilo@gmail.com>
2025-01-14 21:53:15 -0300

    aula 1 de javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,18 @@
+alertar();
+//comentário de uma linha
+/*
+comentário de várias linhas
+*/
+var variavelGlobal = 5;
+
+function alertar() {
+    let variavelLocal = 6;
+    alert('Aproveite a nossa promoção e compre um curso nosso');
+    alert('Tem certeza que não quer comprar?');
+}
+
+function teste() {
+    let variavelTeste = 3;
+    alert('Teste');
+    //texto
+}
