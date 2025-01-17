bec9117120abb96cde921f81b697c8962e48387d
vitorgrilo96 <vitorluizalcantaragrilo@gmail.com>
2025-01-16 22:00:35 -0300

    aula 2 de javascript

--- JavaScript/atividades/atividades.js
+++ JavaScript/atividades/atividades.js
@@ -9,29 +9,73 @@ function exibirMensagem() {
 
 //Exercício 3 - Calculadora
 function realizarCalculos() {
-    let numero1 = 10 , numero2 = 5;
-    somar(numero1,numero2);
-    subtrair(numero1,numero2);
-    dividir(numero1,numero2);
-    multiplicar(numero1,numero2);
+    let numero1 = parseInt(prompt('Informe o primeiro valor'));
+    let numero2 = parseInt(prompt('Informe o segundo valor'));
+    somar(numero1, numero2);
+    subtrair(numero1, numero2);
+    dividir(numero1, numero2);
+    multiplicar(numero1, numero2);
 }
 
 function somar(valor1, valor2) {
     let resultado = valor1 + valor2;
-    console.log('O resultado da soma entre '+valor1+' e '+valor2+' é: '+resultado);
+    console.log('O resultado da soma entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
 }
 
 function subtrair(valor1, valor2) {
     let resultado = valor1 - valor2;
-    console.log('O resultado da subtração entre '+valor1+' e '+valor2+' é: '+resultado);
+    console.log('O resultado da subtração entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
 }
 
 function dividir(valor1, valor2) {
     let resultado = valor1 / valor2;
-    console.log('O resultado da divisão entre '+valor1+' e '+valor2+' é: '+resultado);
+    console.log('O resultado da divisão entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
 }
 
 function multiplicar(valor1, valor2) {
     let resultado = valor1 * valor2;
-    console.log('O resultado da multiplicação entre '+valor1+' e '+valor2+' é: '+resultado);
+    console.log('O resultado da multiplicação entre ' + valor1 + ' e ' + valor2 + ' é: ' + resultado);
+}
+
+//Calculadora "funcional"
+var valor1 = '';
+var valor2 = '';
+var operacao = '';
+const qtdCasaDecimal = 2;
+
+function guardarNumero(valor) {
+    if (!operacao) {
+        valor1 += valor;
+        console.log(valor1);
+    } else {
+        valor2 += valor;
+        console.log(valor2);
+    }
+}
+
+function guardarOperacao(operacaoE) {
+    operacao = operacaoE;
+}
+
+function limpar() {
+    valor1 = '';
+    valor2 = '';
+    operacao = '';
+}
+
+function calcular() {
+    valor1 = parseFloat(valor1);
+    valor2 = parseFloat(valor2);
+    let resultado;
+    if (operacao == '+') {
+        resultado = valor1 + valor2;
+    } else if (operacao == '-') {
+        resultado = valor1 - valor2;
+    } else if (operacao == '*') {
+        resultado = valor1 * valor2;
+    } else if (operacao == '/') {
+        resultado = valor1 / valor2;
+    }
+    alert('Resultado: ' + resultado);
+    limpar();
 }
