a238330c10dd028df07fd8df758bcadf8320cfe3
guirossbach <guilhermerossbach12@gmail.com>
2025-01-21 21:58:07 -0300

    aula 3 js

--- JavaScript/atividades/atividades.js
+++ JavaScript/atividades/atividades.js
@@ -41,7 +41,6 @@ function multiplicar(valor1, valor2) {
 var valor1 = '';
 var valor2 = '';
 var operacao = '';
-const qtdCasaDecimal = 2;
 
 function guardarNumero(valor) {
     if (!operacao) {
@@ -80,12 +79,89 @@ function calcular() {
     limpar();
 }
 
-function verificarSono() {
-    const horasSono = prompt('Informe a quantidade de horas que você dorme em média:');
-    const qtdMinimaHorasSono = 8;
-    if (horasSono < qtdMinimaHorasSono) {
-        alert('Você está dormindo muito pouco.');
+function identificarSono() {
+    const qtdHoras = prompt('Quantas horas você tem dormido por noite?');
+    const horasMinimasSono = 8;
+    if (qtdHoras < horasMinimasSono) {
+        alert('Você está dormindo pouco. Durma mais!');
     } else {
-        alert('Você está dormindo o suficiente. Parabéns!');
+        alert('Você está dormindo o suficiente, continua assim!');
+    }
+}
+
+function horaFormato12() {
+    const qtdHora = prompt('Que horas são?');
+
+    if (qtdHora > 12) {
+        alert(qtdHora - 12 + ' p.m');
+    } else {
+        alert(qtdHora + ' a.m');
+    }
+}
+
+function testarOperadorOu() {
+    const nro = prompt('Informe um digite');
+    if (nro == 5 || nro == 10) {
+        alert('O número é 5 ou 10');
+    } else {
+        alert('Outro número');
+    }
+}
+
+function testarOperadorE() {
+    const idade = prompt('Informe a sua idade');
+    const idadeAmigo = prompt('Informe a idade do seu amigo');
+    if (idade >= 18 && idadeAmigo >= 18) {
+        alert('Vocês estão liberados para beber');
+    } else {
+        alert('Infelizmente não podem beber');
+    }
+}
+
+function validarNumeroEntre(vlInicio, vlFinal, vlValidar) {
+    if (vlValidar >= vlInicio && vlValidar <= vlFinal) {
+        return true;
+    }
+    return false;
+}
+
+function verificarEntradaBalada() {
+    const idade = prompt('Informe a sua idade');
+    const lista = prompt('Você tem nome na lista? Digite Sim ou Não');
+    const ingresso = prompt('Você tem ingresso? Digite Sim ou Não');
+
+    if (idade >= 18 && (ingresso.toUpperCase() == 'SIM' || lista.toUpperCase() == 'SIM')) {
+        alert('Entrada permitida');
+    } else {
+        alert('Entrada negada');
+    }
+}
+
+function verificarEntradaBaladaMelhorado() {
+    if (prompt('Informe a sua idade') >= 18 &&
+        (prompt('Você tem nome na lista? Digite Sim ou Não').toUpperCase() == 'SIM'
+            || prompt('Você tem ingresso? Digite Sim ou Não').toUpperCase() == 'SIM')) {
+        alert('Entrada permitida');
+    } else {
+        alert('Entrada negada');
+    }
+}
+
+function verificarMeiaEntrada() {
+    const idade = prompt('Informe sua idade');
+    if (idade < 12 || idade > 60) {
+        alert('Você tem direito a meia entrada');
+    } else {
+        alert('Você paga entrada inteira');
+    }
+}
+
+function identificarNomeUsuario() {
+    const nomeUsuario = prompt("Informe seu nome");
+    if (!nomeUsuario) {
+        alert("Entrada esta proibida. Obrigatorio informar seu nome!");
+        identificarNomeUsuario();
+    } else {
+        alert("Entrada liberada!");
     }
 }
